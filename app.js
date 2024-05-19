97f8b75e553435f8ac8eb7e2820409c2d442f729
Jamsheed-Khan <jamshedkh365@gmail.com>
2024-05-20 01:36:23 +0500

    working....

---
+++ app.js
@@ -0,0 +1,9 @@
+let toogle = document.getElementById('toggle')
+function toggle(){
+    toogle.classList.add('toogle-nav-open')    
+}
+
+
+function toggle_closs(){
+    toogle.classList.remove('toogle-nav-open')   
+}
